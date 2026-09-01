bfab9c6e9149b69f07372588213bc7142ff879fe
Amol Solase <amolsolse2127@gmail.com>
2026-09-02 01:01:52 +0530

    Time: 7 ms (29.82%), Space: 45.4 MB (24.63%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length/2; i++){
+            int temp = nums[i];
+            nums[i] = nums[nums.length-1-i];
+            nums[nums.length-1-i] = temp;
+        }
+        int elem = 1;
+        int preElem = nums[0];
+        for(int i = 1; i<nums.length; i++){
+            if(nums[i]!=preElem){
+                elem+=1;
+                preElem = nums[i];
+            }
+            if(elem==3){
+                return nums[i];
+            }
+
+        }
+        return nums[0];
+    }
+}
